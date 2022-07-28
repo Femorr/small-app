bf7b026f4dc0dea7c7eff5727ca986b9d6ca0883
Rachel Mawufemor Cudjoe Abede <90472602+Femorr@users.noreply.github.com>
2022-07-28 13:05:50 -0700

    small app

---
+++ small.js
@@ -0,0 +1,12 @@
+console.log("Hello World")
+let sum= 3000 * 548
+console.log("this is the sum:" + sum)
+alert("wrong message")
+var whereIAm = prompt("Were are you")
+
+if(whereIAm == "home"){
+    alert("I'll go to the catholic church")
+}
+else{
+    alert("I'll go to CEYC")
+}
